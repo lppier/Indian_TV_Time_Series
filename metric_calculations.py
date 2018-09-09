924866915207c6c6828bb96a103ef3df63109075
Pier <madstrum@gmail.com>
2018-09-09 14:57:29 +0800

    added metric calculation python notebook to determine > 10 percenters

--- metric_calculations.py
+++ metric_calculations.py
@@ -12,11 +12,22 @@ def mean_absolute_percentage_error(y_true, y_pred):
     return np.mean(np.abs((y_true - y_pred) / y_true)) * 100
 
 
-os.chdir('/Users/pierlim/PycharmProjects/IndianTV')
-df = pd.read_csv("./data/arima111_predicted.csv")
+def find_more_than_10_percent(data):
+    for index, row in data.iterrows():
+        percentage_error = (abs(row['Predicted GRP'] - row['Actual GRP']) / row['Actual GRP']) * 100
+        if percentage_error > 10.0:
+            print("date: {0} error {1}".format(row['GRPRatingsDate'], percentage_error))
 
-mae = mean_absolute_error(df['Actual'], df['Predicted'])
-mse = mean_squared_error(df['Actual'], df['Predicted'])
+
+
+
+os.chdir('/Users/pierlim/PycharmProjects/IndianTV')
+df = pd.read_csv("./data/winter_test.csv")
+# df_train = df.dropna()
+find_more_than_10_percent(df)
+df = df.dropna()
+mae = mean_absolute_error(df['Actual GRP'], df['Predicted GRP'])
+mse = mean_squared_error(df['Actual GRP'], df['Predicted GRP'])
 print('Prediction quality: {:.2f} MSE ({:.2f} RMSE),  ({:.2f} MAE)'.format(mse, math.sqrt(mse), mae))
-mape = mean_absolute_percentage_error(df['Actual'], df['Predicted'])
+mape = mean_absolute_percentage_error(df['Actual GRP'], df['Predicted GRP'])
 print('Prediction quality: {:.2f}% MAPE '.format(mape))
